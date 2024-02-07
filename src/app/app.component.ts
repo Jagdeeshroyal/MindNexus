13cc99d87430571916c18284eac679c78786a09c
jagadeeshroyal <rjroyal2@gmail.com>
2024-02-08 00:19:59 +0530

    completed mind nexus app with full functionallity along with dark and light theme option :) <3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MindNexus';
+export class AppComponent implements OnInit {
+
+
+  isDarkTheme: boolean = false;
+  
+
+  constructor(private themeService : ThemeService) {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+  onThemeChange() {
+   this.isDarkTheme = !this.isDarkTheme;
+   
+  }
+
 }
---
+++ src/app/gpt/gpt.component.ts
@@ -0,0 +1,43 @@
+// gpt.component.ts
+
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { GptService } from '../gpt.service'; // Import the GptService
+
+@Component({
+  selector: 'app-gpt',
+  templateUrl: './gpt.component.html',
+  styleUrls: ['./gpt.component.css']
+})
+export class GptComponent {
+  queryformGroup!: FormGroup;
+  result: any;
+  error: any;
+  @Input()
+  currentTheme: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private gptService: GptService // Inject the GptService
+  ) {}
+
+  ngOnInit() {
+    this.queryformGroup = this.fb.group({
+      query: this.fb.control('')
+    });
+  }
+
+  handleAskGPT() {
+    let message = { role: 'user', content: this.queryformGroup.value.query } ; 
+
+    this.gptService.askGPT(message).subscribe(
+      (resp) => {
+        this.result = resp;
+      },
+      (err) => {
+        this.error = err.error['error']['message'];
+        console.log(err.error['error']);
+      }
+    );
+  }
+}
